92a9a9faab43f36c7b03ed60fa23f8f66c56cdc4
Denner <50850708+DanteWaker@users.noreply.github.com>
2021-05-17 05:23:31 -0300

    CRUD finalizado

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create(name: 'Naruto', motto: 'Born to be wild')
+Dog.create(name: 'Deku', motto: 'Calm as can be')
+Dog.create(name: 'Tatakae', motto: 'Life of the pawty')
+Dog.create(name: 'Subaru', motto: 'Miss Independent')
